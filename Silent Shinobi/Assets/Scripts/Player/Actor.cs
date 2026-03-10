9c4432c03db1399100c309ac6d87f27646fbd69f
CharFarn <charliemarshall1107@gmail.com>
2026-03-10 09:40:05 +0000

    Basic Movement

---
+++ Silent Shinobi/Assets/Scripts/Player/Actor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    int currentHealth;
+    public int maxHealth;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        { Death(); }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
